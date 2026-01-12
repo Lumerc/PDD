e447a357d200832b7b94127e78c3c72c8c7ba6f4
Lumerc <shatalov.boris@mail.ru>
2026-01-12 00:25:16 +0000

    Vite + inertia config

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-blue-600">
+            Добро пожаловать в Laravel PDD!
+        </h1>
+        <p class="mt-4 text-gray-700">
+            Это ваша первая страница на Vue 3 + Inertia.js
+        </p>
+        <p class="mt-2 text-sm text-gray-500">
+            Динамических страниц: 50+
+        </p>
+    </div>
+</template>
+
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+</script>
