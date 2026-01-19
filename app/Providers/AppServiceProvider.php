557766b03e2a333de44eb3a6bb764848ac5b0589
Lumerc <shatalov.boris@mail.ru>
2026-01-19 01:59:13 +0000

    force https on prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Принудительно используем HTTPS в продакшене
+        if ($this->app->environment('production') || 
+            config('app.force_https') === true) {
+            URL::forceScheme('https');
+        }
     }
 }
